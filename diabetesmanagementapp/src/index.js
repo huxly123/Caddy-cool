8973b8cab6f97acc85c6e104688543840d8954fe
huxly123 <huxlygerard5@gmail.com>
2021-11-02 15:35:46 +0530

    index

--- diabetesmanagementapp/src/index.js
+++ diabetesmanagementapp/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BrowseRouter from "react-router-dom"
 
 ReactDOM.render(
+  <BrowseRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowseRouter>,
   document.getElementById('root')
 );
 reportWebVitals();
