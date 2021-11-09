47d189dea905e2e1bd327f033c77c0ab6860e1c4
huxly123 <huxlygerard5@gmail.com>
2021-11-09 18:00:22 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import Pills_front from "./components/Add_pills/Pills_front";
-
+import Caddy from "./components/Caddy_Log/Caddy/Caddy";
 
 function App ()
 {
   return (
     <>
-  <Pills_front/>
+<Pills_front/>
     </>
   );
 }
