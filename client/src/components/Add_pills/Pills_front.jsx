dd84beb44f8326d94e84f9a4e6d3ab1afc017df0
huxly123 <huxlygerard5@gmail.com>
2021-11-09 14:23:47 +0530

    pills

--- client/src/components/Add_pills/Pills_front.jsx
+++ client/src/components/Add_pills/Pills_front.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Footerr from '../Footer/Footerr'
 import Headerr from '../Header/Headerr'
-
+import style from "./pills_front.module.css"
 
 function Pills_front() {
     return (
-        <div>
+        <div className={style.body}>
            <Headerr/>
+           <div className={style.top}>
+<button className={style.but1}>Caddy Log</button>
+
+<button className={style.but2}>My Peers</button>
+           </div>
+           <div className={style.bottom}>
+<button>Add Pills</button>
+
+<button></button>
+           </div>
            <Footerr/>
         </div>
     )
