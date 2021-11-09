aeb20be62e7431841604cb8d4eefebb33b54e7f0
huxly123 <huxlygerard5@gmail.com>
2021-11-09 19:54:02 +0530

    caddy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Pills_front from "./components/Add_pills/Pills_front";
 import Caddy from "./components/Caddy_Log/Caddy/Caddy";
 
+
 function App ()
 {
   return (
     <>
-<Pills_front/>
+<Caddy/>
     </>
   );
 }
--- client/src/components/Caddy_Log/Caddy/Caddy.jsx
+++ client/src/components/Caddy_Log/Caddy/Caddy.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Footerr from '../../Footer/Footerr'
+import Headerr from '../../Header/Headerr'
+import style from "./caddy.module.css"
+
 
 function Caddy() {
     return (
         <div>
-            
+            <Headerr/>
+            <div className={style.caddy}>
+
+<div className={style.top}>
+<div className={style.caddylog}>My Caddy Log</div>
+
+<div className={style.in}><button className={style.pills}>Add Pills</button></div>
+         </div> 
+         
+           </div>
+            <Footerr/>
         </div>
     )
 }
