facc6e6e9dd480620c7e688b5391b47be01691dd
Akash Chandra <chandra.akash020@gmail.com>
2021-11-10 19:59:15 +0530

    v-14

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Pills_front from "./components/Add_pills/Pills_front";
 import { Device } from "./components/Device_connect/main";
 import Footerr from "./components/Footer/Footerr";
 import Headerr from "./components/Header/Headerr";
-import Caddy from "./components/Caddy_Log/Caddy/Caddy";
+import { Caddy } from "./components/Caddy_Log/Caddy/Caddy";
 import { PeopleConnect } from "./components/Community/PeopleConnect";
 import { Main } from "./components/Main";
 import
