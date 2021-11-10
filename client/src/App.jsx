f70faf294a0fa9865c1e41e6ee1a530c78ebceaf
Akash Chandra <chandra.akash020@gmail.com>
2021-11-11 00:08:13 +0530

    v-26 Footer Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import Pills_front from "./components/Add_pills/Pills_front";
 import { Device } from "./components/Device_connect/main";
-import Footerr from "./components/Footer/Footerr";
 import Headerr from "./components/Header/Headerr";
 import { Caddy } from "./components/Caddy_Log/Caddy/Caddy";
 import { PeopleConnect } from "./components/Community/PeopleConnect";
@@ -54,7 +53,7 @@ function App ()
         <div className={styles.footerbody}>
           <Link to="/Main"><div><img src={homeIcon} alt="home"></img></div></Link>
           <Link to="/PeopleConnect"><div><img src={communityIcon} alt="community"></img></div></Link>
-          <Link to="/Caddy"><div><img className={style.fix} src={caddyIcon} alt="caddy"></img></div></Link>
+          <Link to="/Caddy"><div><img className={styles.fix} src={caddyIcon} alt="caddy"></img></div></Link>
         </div>
 
       </div>
--- client/src/components/Add_pills/Pills_front.jsx
+++ client/src/components/Add_pills/Pills_front.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import Footerr from '../Footer/Footerr'
+// import Footerr from '../Footer/Footerr'
 import Headerr from '../Header/Headerr'
 import style from "./pills_front.module.css"
 
-function Pills_front() {
+function Pills_front ()
+{
     return (
         <div className={style.body}>
-           <Headerr/>
-           <div className={style.top}>
-<button className={style.but1}>Caddy Log</button>
+            {/* <Headerr/> */}
+            <div className={style.top}>
+                <button className={style.but1}>Caddy Log</button>
 
-<button className={style.but2}>My Peers</button>
-           </div>
-           <div className={style.bottom}>
-               <div className={style.bottom1}>
-<div className={style.but3}><h4>Add Pills</h4></div>
+                <button className={style.but2}>My Peers</button>
+            </div>
+            <div className={style.bottom}>
+                <div className={style.bottom1}>
+                    <div className={style.but3}><h4>Add Pills</h4></div>
 
-<div className={style.but4}><h4>Add Peers</h4></div>
-</div>
-           </div>
-           <Footerr/>
+                    <div className={style.but4}><h4>Add Peers</h4></div>
+                </div>
+            </div>
+            {/* <Footerr/> */}
         </div>
     )
 }
