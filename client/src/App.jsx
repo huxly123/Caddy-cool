7188224a1d18fa709d547c3e28e532e8b366ac4f
Akash Chandra <chandra.akash020@gmail.com>
2021-11-10 16:52:41 +0530

    v-7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,28 @@ import Headerr from "./components/Header/Headerr";
 import Caddy from "./components/Caddy_Log/Caddy/Caddy";
 import PeopleConnect from "./components/Community/PeopleConnect";
 
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App ()
 {
   return (
     <>
       <Headerr />
-      {/* <Pills_front/> */}
-      <Device />
-      <Footerr />
-      {/* <Caddy/> */}
 
-      {/* <PeopleConnect /> */}
+      {/* <Pills_front/> */}
+      {/* <Device /> */}
+      {/* <Caddy/> */}
+      <PeopleConnect />
+
+      <Footerr />
+
     </>
   );
 }
