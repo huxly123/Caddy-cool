42e1a527219985cbbdaf52a54abd2f7be896b234
huxly123 <huxlygerard5@gmail.com>
2021-11-11 15:58:12 +0530

    router

--- client/src/components/Caddy_Log/Iframe_addpill/Iframe_addpill.jsx
+++ client/src/components/Caddy_Log/Iframe_addpill/Iframe_addpill.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import style from "./iframe_addpill.module.css"
 
-import
-{
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function Iframe_addpill() {
     return (
        <>
-       <Router>
-           <Switch>
-         
-           </Switch>
-       </Router>
+     
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, iusto omnis. Tempore earum molestias iusto, officiis quaerat nesciunt similique repudiandae reprehenderit dolores est, perferendis unde ea dignissimos harum fugit expedita!</p>
 
