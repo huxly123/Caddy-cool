0c42441f515e9beda69173ccd78c5c137b613752
Kamlesh <kamlesh_fw11_179@masai.school>
2021-11-11 16:53:19 +0530

    Server update

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const connect = require("./config/db")
 const userCotroller = require("./controllers/user.controller")
+const {register,login} = require("./controllers/auth.controller")
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-
+app.post("/register",register)
+app.post("/login",login);
 app.use("/user",userCotroller)
 
 
