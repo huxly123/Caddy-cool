482d62bb9dc6e2746447cc2b1a69218ab818f5ef
huxly123 <huxlygerard5@gmail.com>
2021-11-11 12:50:56 +0530

    iframe

---
+++ client/src/components/Caddy_Log/Iframe_addpill/Iframe_addpill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./iframe_addpill.module.css"
+
+function Iframe_addpill() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Iframe_addpill
---
+++ client/src/components/Caddy_Log/Iframe_showpill/Iframe_showpill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./iframe_showpill.css"
+
+function Iframe_showpill() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Iframe_showpill
