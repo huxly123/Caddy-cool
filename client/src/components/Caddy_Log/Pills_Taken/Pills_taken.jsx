9e3d653a4dd5530a465ee599d6e1ae4baec58b0e
huxly123 <huxlygerard5@gmail.com>
2021-11-11 12:30:13 +0530

    caddy-update

--- client/src/components/Caddy_Log/Pills_Taken/Pills_taken.jsx
+++ client/src/components/Caddy_Log/Pills_Taken/Pills_taken.jsx
@@ -6,8 +6,9 @@ function Pills_taken() {
     return (
        <div className={style.body}>
             <div className={style.flexx}>
-            <div style={{fontWeight:"bolder"}}>Pills Taken</div>
-            <div><h6>Date -</h6></div>
+            <div className={style.pillsfont}>Pills Taken</div>
+            <div><h6 className={style.date}>Date-25/02/98</h6>
+           <button className={style.search}>Search</button></div>
         </div>
 
         <div className={style.greenline}></div>
