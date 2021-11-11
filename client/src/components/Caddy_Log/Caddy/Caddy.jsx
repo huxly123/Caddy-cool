db9c1bc84182fdc66e6d9adcf95205d48a491f1c
huxly123 <huxlygerard5@gmail.com>
2021-11-11 15:26:40 +0530

    setframe

--- client/src/components/Caddy_Log/Caddy/Caddy.jsx
+++ client/src/components/Caddy_Log/Caddy/Caddy.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import style from "./caddy.module.css"
 import Pills_remaining from '../Pills_Remaining/Pills_remaining'
@@ -7,6 +7,13 @@ import Pills_taken from '../Pills_Taken/Pills_taken'
 
 function Caddy ()
 {
+
+const [frame,setframe]=useState(false)
+
+const handletrue=()=>{
+    setframe(true)
+}
+
     return (
         <div className={style.body}>
             {/* <Headerr /> */}
@@ -15,20 +22,27 @@ function Caddy ()
                 <div className={style.top}>
                     <div className={style.caddylog}>My Caddy Log</div>
 
-                <div className={style.in}><Link to="/add_pill"><button className={style.pills}>Add Pills</button></Link></div>
+                <div className={style.in}><button onClick={handletrue} className={style.pills}>Add Pills</button></div>
 
                 </div>
                 <div className={style.line}></div>
 
+               
 
 <Pills_remaining/>
+{frame?
+<iframe className={style.iframee} src="/add_pill"></iframe>:null
+
+}
 <div className={style.wrapper}>
 <Pills_taken/>
 
 </div>
             </div>
             {/* <Footerr /> */}
+        
         </div>
+
     )
 }
 
