303bbe5ec6fdc231e5c599733a127a6a048fc5af
Kamlesh <kamlesh_fw11_179@masai.school>
2021-11-11 16:55:29 +0530

    Updated the api routes

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -8,36 +8,14 @@ const router = express.Router();
 router.get("",async(req,res)=>{
     const data = await User.find({});
     return res.send(data)
-    console.log(data)
 })
 
 
-router.post("",
-body("name").not().isEmpty().isLength({min:5}).withMessage("Please enter a valid Name."),
-body('email').not().isEmpty().custom(value=>{
-    return User.find({email:{$eq:value}}).then(user=>{
-        if(user){
-            return Promise.reject("Email already in use.")
-        }
-    })
-}),
-body('password').not().isEmpty().isLength({min:6}).withMessage("Please enter a valid Password."),
-async(req,res)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        let array = []
-        errors.array().forEach(e => array.push(e.msg))
-        res.json({
-            method:req.method,
-            status:400,
-            errors:array
-        })
-
-    }else{
+router.post("",async(req,res)=>{
     const data = await User.create(req.body);
-    res.send(data);
-    }
+    return res.send(data)
 })
+
 
 router.get("/:id",async(req,res)=>{
     const data = await User.findById(req.params.id);
