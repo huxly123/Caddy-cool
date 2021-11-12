bc495f65ef39134bf5657c5bd326ddb825b74f4f
Akash Chandra <chandra.akash020@gmail.com>
2021-11-12 11:17:54 +0530

    12-11-21 group chat v-5

---
+++ client/src/components/Community/Groups/ActiveGroupLists.jsx
@@ -0,0 +1,13 @@
+import style from "./ActiveGroups.module.css";
+import { Link } from "react-router-dom";
+
+function ActiveGroupLists ()
+{
+    return (
+        <div>
+            This is list
+        </div>
+    )
+}
+
+export { ActiveGroupLists }
--- client/src/components/Community/Groups/ActiveGroups.jsx
+++ client/src/components/Community/Groups/ActiveGroups.jsx
@@ -1,39 +1,53 @@
 import style from "./ActiveGroups.module.css";
-import { Link } from 'react-router-dom'
+import
+{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { ActiveGroupLists } from "./ActiveGroupLists";
 
 function ActiveGroups ()
 {
     return (
-        <>
-            <div className={style.top}>
-                <Link to="/IndividualGroupMembers">
-                    <span className={style.btnLayout1}>
+        <Router>
+            <>
+                <div className={style.top}>
+                    <Link to="/ActiveGroupLists">
+                        <span className={style.btnLayout1}>
+                            <span className={style.headBtn}>
+                                Active Groups
+                            </span>
+                        </span>
+                    </Link>
+
+                    {/* <Link to="/IndividualGroupMembers"> */}
+                    <span className={style.btnLayout2}>
                         <span className={style.headBtn}>
-                            Active Groups
+                            Activities Planned
                         </span>
                     </span>
-                </Link>
+                    {/* </Link> */}
 
-                {/* <Link to="/IndividualGroupMembers"> */}
-                <span className={style.btnLayout2}>
-                    <span className={style.headBtn}>
-                        Activities Planned
-                    </span>
-                </span>
-                {/* </Link> */}
-
-                {/* <span className={style.btn2}>Activities Planned</span> */}
-            </div>
-            {/* <div className={style.bottom}>
-                <div className={style.bottom1}>
+                    {/* <span className={style.btn2}>Activities Planned</span> */}
+                </div>
+                <div className={style.bottom}>
+                    <Switch>
+                        <Route path="/ActiveGroupLists" exact>
+                            <ActiveGroupLists />
+                        </Route>
+                    </Switch>
+                    {/* <div className={style.bottom1}>
                     <Link to="/IndividualGroupMembers"><div className={style.btn3}><h5>Choose Group Manually</h5></div></Link>
 
                     <div className={style.btn4}><h5>Get Added in a group randomly</h5></div>
 
                     <div className={style.btn5}><h5>Create a group</h5></div>
+                </div> */}
                 </div>
-            </div> */}
-        </>
+            </>
+        </Router>
     )
 }
 
