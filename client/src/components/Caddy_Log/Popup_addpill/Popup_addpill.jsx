4e735d373cab2ff8f5d739b4e637fabf8520cbc0
huxly123 <huxlygerard5@gmail.com>
2021-11-12 13:53:02 +0530

    mock-server

--- client/src/components/Caddy_Log/Popup_addpill/Popup_addpill.jsx
+++ client/src/components/Caddy_Log/Popup_addpill/Popup_addpill.jsx
@@ -4,6 +4,17 @@ import close from "./close.svg"
 
 
 function Popup_addpill({setbtn,btn}) {
+
+const handleSubmit=(e)=>{
+e.preventDefault();
+
+
+}
+
+const handleChange=(e)=>{
+
+}
+
     return (btn)?(
        <>
      <div className={style.popupbody}>
@@ -15,27 +26,26 @@ function Popup_addpill({setbtn,btn}) {
          }}></img>
 
 </div>
-
-<input className={style.pillinput} placeholder="Pill Name"></input>
+<form onSubmit={handleSubmit}>
+<input className={style.pillinput} name="pill_name" placeholder="Pill Name"></input>
 
 <h5 className={style.startdate}>Start Date</h5>
 
-<input className={style.dateinput} placeholder="Enter Date"></input>
+<input className={style.dateinput} name="date" placeholder="Enter Date"></input>
 
 <h5 className={style.duetime}>Due Time for Consumption</h5>
 
 <div className={style.consumeflex}>
-<input className={style.consumeinput} placeholder="Enter Time"></input>
+<input className={style.consumeinput} name="time" placeholder="Enter Time"></input>
 
-<select>
-    <option>AM</option>
-    <option>PM</option>
+<select >
+    <option value="AM">AM</option>
+    <option value="PM">PM</option>
 </select>
 </div>
 
-<button className={style.finishbut} onClick={()=>{
-    setbtn(false)
-}}>Add</button>
+<button className={style.finishbut} type="submit">Add</button>
+</form>
      </div>
 
 
