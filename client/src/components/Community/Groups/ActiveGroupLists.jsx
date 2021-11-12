3abb3f7ad30dbb41919ab77a0439a7bea134a397
Akash Chandra <chandra.akash020@gmail.com>
2021-11-12 15:13:20 +0530

    12-11-21 group chat v-9

--- client/src/components/Community/Groups/ActiveGroupLists.jsx
+++ client/src/components/Community/Groups/ActiveGroupLists.jsx
@@ -4,53 +4,76 @@ import { Link } from "react-router-dom";
 function ActiveGroupLists ()
 {
     return (
-        <>
-            <div className={style.divs}>
-                <div className={style.divsHead}>
-                    <span className={style.divsHeadLeftLayout}>
-                        <span className={style.divsHeadLeftElement}>
-                            #TheLaughGang
-                        </span>
-                    </span>
-                    <span className={style.divsHeadRightLayout}>
-                        <span className={style.divsHeadRightElement}>
-                            Members - 18
-                        </span>
-                    </span>
-                </div>
-            </div>
+        <div style={{ maxHeight: "600px", overflowY: "scroll" }}>
 
-            <div className={style.divs}>
-                <div className={style.divsHead}>
-                    <span className={style.divsHeadLeftLayout}>
-                        <span className={style.divsHeadLeftElement}>
-                            #TheOGs
-                        </span>
-                    </span>
-                    <span className={style.divsHeadRightLayout}>
-                        <span className={style.divsHeadRightElement}>
-                            Members - 18
-                        </span>
-                    </span>
-                </div>
-            </div>
 
-            <div className={style.divs}>
-                <div className={style.divsHead}>
-                    <span className={style.divsHeadLeftLayout}>
-                        <span className={style.divsHeadLeftElement}>
-                            #F.R.I.E.N.D.S
-                        </span>
-                    </span>
-                    <span className={style.divsHeadRightLayout}>
-                        <span className={style.divsHeadRightElement}>
-                            Members - 18
-                        </span>
-                    </span>
-                </div>
-            </div>
+            <div style={{ height: "800px" }}>
 
-        </>
+                <div className={style.divs}>
+                    <div className={style.divsHead}>
+                        <span className={style.divsHeadLeftLayout}>
+                            <span className={style.divsHeadLeftElement}>
+                                #TheLaughGang
+                            </span>
+                        </span>
+                        <span className={style.divsHeadRightLayout}>
+                            <span className={style.divsHeadRightElement}>
+                                Members - 18
+                            </span>
+                        </span>
+                    </div>
+
+                    <div className={style.joinButton}>
+                        <span>
+                            Join Group
+                        </span>
+                    </div>
+                </div>
+
+                <div className={style.divs}>
+                    <div className={style.divsHead}>
+                        <span className={style.divsHeadLeftLayout}>
+                            <span className={style.divsHeadLeftElement}>
+                                #TheOGs
+                            </span>
+                        </span>
+                        <span className={style.divsHeadRightLayout}>
+                            <span className={style.divsHeadRightElement}>
+                                Members - 18
+                            </span>
+                        </span>
+                    </div>
+
+                    <div className={style.joinButton}>
+                        <span>
+                            Join Group
+                        </span>
+                    </div>
+                </div>
+
+                <div className={style.divs}>
+                    <div className={style.divsHead}>
+                        <span className={style.divsHeadLeftLayout}>
+                            <span className={style.divsHeadLeftElement}>
+                                #F.R.I.E.N.D.S
+                            </span>
+                        </span>
+                        <span className={style.divsHeadRightLayout}>
+                            <span className={style.divsHeadRightElement}>
+                                Members - 18
+                            </span>
+                        </span>
+                    </div>
+
+                    <div className={style.joinButton}>
+                        <span>
+                            Join Group
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
