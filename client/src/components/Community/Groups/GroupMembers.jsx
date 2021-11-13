87373160d676ed283a69dc6d543212308d11d8d9
Akash Chandra <chandra.akash020@gmail.com>
2021-11-13 09:33:08 +0530

    update 13-11-21 9:33:08am

--- client/src/components/Community/Groups/GroupMembers.jsx
+++ client/src/components/Community/Groups/GroupMembers.jsx
@@ -1,12 +1,24 @@
-import style from "./GroupMembers.css";
+import style from "./GroupMembers.module.css";
+import memberLists from "../svg/memberLists.svg"
 
 function GroupMembers ()
 {
 
     return (
-        <div>
-            List of group members
-        </div>
+        <>
+            <div className={style.heading}>
+                #TheLaughGang
+            </div>
+            <div className={style.divButtonLayout}>
+                <div className={style.divButton}>
+                    Join Group
+                </div>
+            </div>
+            <div>
+                <img src={memberLists} alt="" />
+            </div>
+        </>
+
     )
 }
 
