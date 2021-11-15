786924e6c5a77017f1187a6bf110b03218ed4757
huxly123 <huxlygerard5@gmail.com>
2021-11-15 22:03:17 +0530

    creatingbackendforpills

---
+++ server/src/models/taken.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const takenSchema = new mongoose.Schema(
+  {
+    pill_name: { type: String, required: true },
+    date: { type: String, required: true },
+    time: { type: Number, required: true },
+    when: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Taken = mongoose.model("taken", takenSchema);
+
+module.exports = Taken;
